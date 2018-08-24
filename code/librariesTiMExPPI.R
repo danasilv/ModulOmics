f24e4b5f87ade0fa95c94df11f28df640e93ca42
Dana Silverbush <32930322+danasilv@users.noreply.github.com>
2018-08-24 13:22:51 -0400

    Update librariesTiMExPPI.R
    Update librariesTiMExPPI.R so to install a package if it is not installed

--- code/librariesTiMExPPI.R
+++ code/librariesTiMExPPI.R
@@ -1,4 +1,14 @@
-library("TiMEx")
-library("igraph")
-library("cplexAPI")
-library("gtools")
+pkgLoad <- function(x)
+{
+  if (!require(x, character.only = TRUE))
+  {
+    install.packages(x, dependencies = TRUE, repos = "http://cran.us.r-project.org")
+    if (!require(x, character.only = TRUE))
+      stop("Package not found")
+  }
+}
+
+pkgLoad("TiMEx")
+pkgLoad("igraph")
+pkgLoad("cplexAPI")
+pkgLoad("gtools")

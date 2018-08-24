9d27f1a2816e6c941b7eb337f2ccb7e35a3fed6f
Dana Silverbush <32930322+danasilv@users.noreply.github.com>
2018-08-24 13:25:42 -0400

    Update runModulOmics.R
    Now assumes that all files are under the current directory

--- code/runModulOmics.R
+++ code/runModulOmics.R
@@ -1,18 +1,22 @@
-rm(list=ls())
-
-# Change parameters:
+# Change parameters if needed (currently assumes that all files are under the current directory):
 
 # format: column names are gene followed by either "-Mut" or "-CNA, rownames are patients. Data as downloaded from TCGA
-mutInput<-"/home/pace/bnet/danasilv/hitME/upload/SlimInput/mut.txt"
+mutInput <- "SlimInput/mut.txt"
 # format: column names are genes, rownames are patients. Data as downloaded from TCGA
-zScoreInput<-"/home/pace/bnet/danasilv/hitME/upload/SlimInput/zScore.txt"
-pathOutput<-"/home/pace/bnet/danasilv/hitME/upload/output/"
-pathCode<-"/home/pace/bnet/danasilv/hitME/upload/code/"
-PPIInput<-"/home/pace/bnet/danasilv/hitME/upload/SlimInput/PPI_SP.txt"
-TRRUSTInput<-"/home/pace/bnet/danasilv/hitME/upload/SlimInput/TRRUST.txt"
+zScoreInput <- "SlimInput/zScore.txt"
+pathOutput <- "output/"
+pathCode <- "code/"
+PPIInput <- "SlimInput/PPI_SP.txt"
+TRRUSTInput <- "SlimInput/TRRUST.txt"
 
-source(paste(pathCode,"callPipeSP.R",sep=""))
+source(paste(pathCode, "callPipeSP.R", sep = ""))
 
 # The function reads cancer cohort and computes ModuleOmics scores
-results<-run_ModulOmics(mutInput, zScoreInput, PPIInput, TRRUSTInput, pathCode, pathOutput)
+results <-
+  run_ModulOmics(mutInput,
+                 zScoreInput,
+                 PPIInput,
+                 TRRUSTInput,
+                 pathCode,
+                 pathOutput)
   
